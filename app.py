db1487a96b47c28bcef022f08794d3316ff66fa7
unknown <nthjs195@yahoo.co.jp>
2022-02-10 22:26:39 +0900

    URLの一部を変数として受け取る

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def Hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route('/user/<name>')
+def greet(name):
+    return name + 'さん、こんばんわ！' 
+
 #これより下には何もかかない
 if __name__== '__main__':
   app.run()
