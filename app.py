674f5ccc98a01d81104d1fe308b7cc96314c9c41
unknown <nthjs195@yahoo.co.jp>
2022-02-10 22:54:03 +0900

    テンプレートを表示させる

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import turtle
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  
 
@@ -12,12 +12,12 @@ def greet(name):
     return name + 'さん、こんばんわ！' 
     #[RETUN　文字列]の形にする必要がある
 
-#テンプレートを使う
+#from flask import Flask, render_template
+
+# テンプレートを使う
 @app.route('/test')
 def test():
     return render_template('index.html')
-    
-
     
 #これより下には何もかかない
 if __name__== '__main__':
