94b5d846a5f746d4c7f22ad7b86b825e6af68900
unknown <nthjs195@yahoo.co.jp>
2022-02-14 21:47:09 +0900

    テスト

--- app.py
+++ app.py
@@ -1,81 +1,50 @@
-# flaskからflaskの機能を引っ張ってきてねという命令文
-import sqlite3,random
-from flask import Flask , render_template
-from sympy import Add 
+import sqlite3
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def top():
-    return "はろーふらすく！ここはトップページです！"
+@app.route("/helloworld")  # URLを決めているもの
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-@app.route("/hello/<name>")
-def hello(name):
-    return name + "さん、こんにちは"
 
-# 変数の受け渡し
-@app.route("/test")
+@app.route('/user/<name>')  # URLの中に<>を使うと変数としてアプリで受け取れる
+def greet(name): # 変数として受け取る者はメソッド引数に書く
+    return name + 'さん、こんばんは！'
+    # 「return 文字列」の形にする必要がある
+
+
+# テンプレートを使う
+@app.route('/test')
 def test():
-    py_name = "にんじゃわんこ"
-    py_age = 14
-    return render_template("index.html",html_name = py_name ,html_age = py_age)
+    return render_template("index.html")
+    # 上の実行結果と下の実行結果は同じ
+    # return '<!DOCTYPE html> <html lang="ja"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Flaskテスト</title> </head> <body> <h1>Flask、こんばんは！</h1> <p>テンプレートを使ってます！</p> </body> </html>'
 
-# 変数の受け渡し練習課題
-# @app.route("/color")
-# def color():
-#     py_color = "黄色"
-#     return render_template("color.html",html_color = py_color)
 
-# データベースとの接続練習
-# @app.route("/color")
-# def color():
-#     # 「sqlite3でcolor.dbに接続してね」ということをconnに代入
-#     conn = sqlite3.connect("color.db")
-#     # 「sqlite3でcolor.dbに接続し、操作できるようにしてね」という事をcに代入
-#     c = conn.cursor()
-#     #「()の中のSQL文を実行してね」
-#     c.execute("SELECT name FROM colors WHERE id = 1;")
-#     py_color = c.fetchone() 
-#     c.close()
-#     return render_template("color.html",html_color = py_color)
+# テンプレートに変数を渡す
+@app.route('/test_val')
+def test_val():
+    py_name = 'にんじゃわんこ' # 変数
+    py_age = 20 # 変数
+    return render_template('index_val.html', name=py_name, age=py_age)
 
-#データベースから全件取得してランダムで表示
-@app.route("/color")
-def color():
-    # 「sqlite3でcolor.dbに接続してね」ということをconnに代入
-    conn = sqlite3.connect("color.db")
-    # 「sqlite3でcolor.dbに接続し、操作できるようにしてね」という事をcに代入
-    c = conn.cursor()
-    #「()の中のSQL文を実行してね」
-    c.execute("SELECT * FROM colors;")
-    py_color = c.fetchall() 
-    py_color =random.choice(py_color)
-    print(py_color)
-    c.close()
-    return render_template("color.html",html_color = py_color)
 
-@app.route("/list")
+@app.route('/list')
 def task_list():
+    # 「sqlite3でflask_test.dbに接続してね」ということをconnに代入
     conn = sqlite3.connect("flask_test.db")
+    # 「sqlite3で接続したものを操作してね」ということをcに代入
     c = conn.cursor()
+    # ()内のSQL文を実行してね
     c.execute("SELECT * FROM tasks;")
-    # データの整形
+    # 検索結果のデータを取得
     task_list = []
+    #print('fetchall()の実行結果',c.fetchall())
     for row in c.fetchall():
         task_list.append({"id":row[0],"task":row[1]})
-    print(task_list)
+    # データベースと接続解除
     c.close()
-    return render_template("task_list.html")
-　　新しいルートを作る名前は/Add
-　　関数の戻り値はとしてadd.
+    return render_template("task_list.html",task_list=task_list)
 
-
-
-     base.htmlと言うファイルを作っておいてください（中はからでもＯＫです）
-
-
-# メインファイルだった時のみpythonを実行
-if __name__ == "__main__":
-    # アプリを実行
-    app.run(debug=True)
